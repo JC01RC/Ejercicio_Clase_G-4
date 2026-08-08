2d068e4176061d674c299ded73a0d10b882b7598
Administrador Programas <admon.programa@bomohsa.com>
2026-08-07 20:27:36 -0600

    Configuración inicial del proyecto con Express

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("API funcionando correctamente.");
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`Servidor ejecutándose en http://localhost:${PORT}`);
+});
